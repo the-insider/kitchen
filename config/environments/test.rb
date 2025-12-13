9f0c0ffffecd10d1d7b427b0af85847a4ce22022
Gowtham <gwthm.in@gmail.com>
2025-12-14 04:40:03 +0530

    Set new environment for QA

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,7 +57,4 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Allow fly.io for demo in test
-  config.hosts << '.fly.dev'
 end
