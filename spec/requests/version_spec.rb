fa617c193cbbdda55f40f1717a84ddbb3bd860c4
Gowtham <gwthm.in@gmail.com>
2025-12-14 18:04:51 +0530

    Fix version spec lint issues

--- spec/requests/version_spec.rb
+++ spec/requests/version_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Version API', type: :request do
+RSpec.describe 'Version API' do
   describe 'GET /version' do
     it 'returns version information' do
       get '/version'
@@ -8,7 +8,7 @@ RSpec.describe 'Version API', type: :request do
       expect(response).to have_http_status(:success)
       expect(response.content_type).to include('application/json')
 
-      json_response = JSON.parse(response.body)
+      json_response = response.parsed_body
       expect(json_response).to have_key('rails_env')
       expect(json_response).to have_key('build_version')
       expect(json_response['rails_env']).to eq(Rails.env)
@@ -23,7 +23,7 @@ RSpec.describe 'Version API', type: :request do
       it 'returns the BUILD_VERSION' do
         get '/version'
 
-        json_response = JSON.parse(response.body)
+        json_response = response.parsed_body
         expect(json_response['build_version']).to eq('v1.2.3-20241214120000')
       end
     end
@@ -37,10 +37,9 @@ RSpec.describe 'Version API', type: :request do
       it 'returns unknown for build_version' do
         get '/version'
 
-        json_response = JSON.parse(response.body)
+        json_response = response.parsed_body
         expect(json_response['build_version']).to eq('unknown')
       end
     end
   end
 end
-
